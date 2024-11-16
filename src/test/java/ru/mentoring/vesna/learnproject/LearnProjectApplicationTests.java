2713651edfc05d746ca8523ded6b6668b967a231
rb078081 <Andrey.Gurzhi@rosbank.ru>
2024-11-16 12:06:10 +0300

    init commit

---
+++ src/test/java/ru/mentoring/vesna/learnproject/LearnProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mentoring.vesna.learnproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
