5dc1ffc6b56d4bd6686337f8cdcba12be325f20e
V1V2V3V <vesna05olga@gmail.com>
2024-12-02 11:06:30 +0300

    Task 1 (#1)
    * Task 1
    Получение животных по имени
    
    * Рефакторинг

--- src/main/java/ru/mentoring/vesna/learnproject/controller/AnimalController.java
+++ src/main/java/ru/mentoring/vesna/learnproject/controller/AnimalController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.mentoring.vesna.learnproject.model.Animal;
 import ru.mentoring.vesna.learnproject.service.AnimalService;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/animals")
 public class AnimalController {
@@ -18,7 +20,7 @@ public class AnimalController {
     private final AnimalService animalService;
 
     @Autowired
-    public AnimalController( AnimalService animalService ) {
+    public AnimalController(AnimalService animalService) {
         this.animalService = animalService;
     }
 
@@ -32,5 +34,14 @@ public class AnimalController {
     public ResponseEntity<Animal> getAnimalById( @PathVariable Long id ) {
         Animal animal = animalService.getAnimalById( id );
         return ResponseEntity.ok( animal );
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<List<Animal>> getAnimalByName(@PathVariable String name) {
+        List<Animal> animalEntities = animalService.getAnimalsByName(name);
+        if (animalEntities.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(animalEntities);
     }
 }
--- src/main/java/ru/mentoring/vesna/learnproject/service/AnimalService.java
+++ src/main/java/ru/mentoring/vesna/learnproject/service/AnimalService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import ru.mentoring.vesna.learnproject.jpa.repository.AnimalRepository;
 import ru.mentoring.vesna.learnproject.model.Animal;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class AnimalService {
@@ -12,11 +14,18 @@ public class AnimalService {
     private final AnimalRepository animalRepository;
 
 
-    public Animal createAnimal( Animal animal ) {
-        return Animal.fromEntity( animalRepository.save( animal.toEntity() ) );
+    public Animal createAnimal(Animal animal) {
+        return Animal.fromEntity(animalRepository.save(animal.toEntity()));
     }
 
-    public Animal getAnimalById( Long id ) {
-        return Animal.fromEntity( animalRepository.findById( id ).orElseThrow() );
+    public Animal getAnimalById(Long id) {
+        return Animal.fromEntity(animalRepository.findById(id).orElseThrow());
+    }
+
+    public List<Animal> getAnimalsByName(String name) {
+        return animalRepository.findByName(name)
+                .stream()
+                .map(Animal::fromEntity)
+                .toList();
     }
 }
